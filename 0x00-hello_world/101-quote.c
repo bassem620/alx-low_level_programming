4a3e310d2c20fb78bea63b7881442ab535207ae7
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-10-06 01:34:06 +0300

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
-#include <unistd.h>
-
+#include <stdio.h>
+/**
+   * main - prints to the standard error
+   *
+   * Return: one
+   */
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
-
