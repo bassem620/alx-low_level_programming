5f17057c262678adb98005900820c35788b44b5a
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-10-08 03:21:03 +0300

    .

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-    int first_digit, second_digit;
+    int x, y;
 
-    for (first_digit = 0; first_digit <= 8; first_digit++)
+    for (x = 0; x <= 8; x++)
     {
-        for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+        for (y = x + 1; y <= 9; y++)
         {
-            putchar(first_digit + '0');
-            putchar(second_digit + '0');
+            putchar(x + '0');
+            putchar(y + '0');
 
-            if (first_digit != 8 || second_digit != 9)
+            if (x != 8 || y != 9)
             {
                 putchar(',');
                 putchar(' ');
